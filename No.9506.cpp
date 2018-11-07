0f885fc5edf25b8ff3f8075fba82897d827b31d9
CheeseBall <2489ckckck@naver.com>
2018-11-07 11:57:20 +0900

    modified

--- No.9506.cpp
+++ No.9506.cpp
@@ -20,7 +20,7 @@ int main(){
 				cout << " + " << v[i];
 			cout << endl;
 		}
-		else cout << n <<"is NOT perfect."<<endl;
+		else cout << n <<" is NOT perfect."<<endl;
 			
 		v.clear();
 		sum = 1;
